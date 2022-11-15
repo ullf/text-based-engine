0caa7077bc7bcc6c9bd6cc4167e1601ab4d23329
ullf <sokomark1412@gmail.com>
2022-11-15 19:35:02 +0200

    initial

--- structure/quest.go
+++ structure/quest.go
@@ -4,25 +4,24 @@ package structure
 0 - lookup
 1 - WalkTo
 */
-var listOfActions []int = []int{0,1,2,3}
+var listOfActions []int = []int{0, 1, 2, 3}
 
 type action struct {
 	HeroName string
-	Action int
+	Action   int
 }
 
 type questStep struct {
 	QuestId int
-	Hero *hero
-	Action *action
-
+	Hero    *hero
+	Action  *action
 }
 
 type quest struct {
-	QuestId int
-	Name string
+	QuestId     int
+	Name        string
 	Description string
-	Step []questStep
+	Step        []questStep
 }
 
 type questFunctions interface {
@@ -35,15 +34,13 @@ func CreateAction(name string, todo int) action {
 	return *act
 }
 
-func CreateSteps(h *hero,a *action) questStep {
-	s := &questStep{Hero: h,Action: a}
+func CreateSteps(h *hero, a *action) questStep {
+	s := &questStep{Hero: h, Action: a}
 
 	return *s
 }
 
-func CreateQuest(name string, description string,steps []questStep) quest {
-	q := &quest{ Name: name, Description: description, Step: steps }
-	
+func CreateQuest(name string, description string, steps []questStep) quest {
+	q := &quest{Name: name, Description: description, Step: steps}
 	return *q
 }
-
